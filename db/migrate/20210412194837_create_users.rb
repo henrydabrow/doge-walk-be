c189fa68cbb74bcee049aa268deb9c8747023e6e
Henryk Dabrowski <henry.dabrow@gmail.com>
2021-04-12 21:57:03 +0200

    Migrade from int ID's to UUID

--- db/migrate/20210412194837_create_users.rb
+++ db/migrate/20210412194837_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
+    create_table :users, id: :uuid do |t|
       t.string :email
       t.string :password_digest
+      t.text :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_27_191105) do
+ActiveRecord::Schema.define(version: 2021_04_12_194837) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
