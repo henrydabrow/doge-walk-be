7c92c86cc0586d3bcffe27d9528f579fcc6e3fba
Henryk Dabrowski <henry.dabrow@gmail.com>
2021-04-16 00:24:19 +0200

    Add email and password validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
 
   validates_presence_of :email
   validates_uniqueness_of :email
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :password,
+            length: { minimum: 8 },
+            format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W)[A-Za-z\d\W]{8,}\z/ },
+            allow_blank: true
 end
