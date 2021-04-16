5646fc5fd29f7285e6ddc5e433f1afb61bc40729
Henryk Dabrowski <henry.dabrow@gmail.com>
2021-04-17 00:31:16 +0200

    Add expiration to jwt payload

--- app/controllers/api/v1/helpers/auth.rb
+++ app/controllers/api/v1/helpers/auth.rb
@@ -5,7 +5,9 @@ module API
     module Helpers
       module Auth
         def generate_token(payload)
-          JWT.encode(payload, private_key, "RS256")
+          payload_with_expiration = add_expiration(payload)
+
+          JWT.encode(payload_with_expiration, private_key, "RS256")
         end
 
         def verify_token!
@@ -40,6 +42,17 @@ module API
 
         def token_expired!
           error!({ errors: ["Expired token"] }, 401)
+        end
+
+        def add_expiration(payload)
+          jwt_validity_in_hours = ENV["JWT_VALIDITY_IN_HOURS"].to_i
+          expiration_timestamp  = (Time.now + jwt_validity_in_hours.hours).to_i
+
+          payload.merge(
+            {
+              exp: expiration_timestamp
+            }
+          )
         end
       end
     end
