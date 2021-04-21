947ce0dd5ad3105caab0b04b93e074f35b2d4f0d
Henryk Dabrowski <henry.dabrow@gmail.com>
2021-04-21 23:57:29 +0200

    Add GET /pets endpoint

---
+++ app/controllers/api/v1/pets/index.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module API
+  module V1
+    module Pets
+      class Index < Base
+        desc "Shows all pets"
+
+        params do
+          optional :page,     type: Integer
+          optional :per_page, type: Integer
+        end
+
+        get do
+          # binding.pry
+          render_paginated Pet
+        end
+      end
+    end
+  end
+end
