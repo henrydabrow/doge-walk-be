c7850a8ef20bb1866ea761afdb7aaa297556f842
Henryk Dabrowski <henry.dabrow@gmail.com>
2021-05-09 15:41:49 +0200

    Add relation between User and Refresh models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :pets, dependent: :destroy
+  has_one :refresh_token, dependent: :destroy
 
   validates_presence_of :email
   validates_uniqueness_of :email
