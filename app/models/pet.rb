5b9a638b9e8d784752196b9c400ed7ea4cae199b
Henryk Dabrowski <henry.dabrow@gmail.com>
2021-05-18 23:48:15 +0200

    Add association of an image to a pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,6 +3,8 @@
 class Pet < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: :user_id
 
+  has_one_attached :image
+
   validates_presence_of :name
   validates_presence_of :kind
 
