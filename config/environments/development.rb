1334a3f4af88e5be08e906bdddba652bedf8da02
Henryk Dabrowski <henry.dabrow@gmail.com>
2021-05-20 23:36:12 +0200

    Fix image url prefix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
-  Rails.application.routes.default_url_options[:host] = "localhost:3000"
+  config.action_mailer.default_url_options = { host: "localhost:4000" }
+  Rails.application.routes.default_url_options[:host] = "localhost:4000"
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
